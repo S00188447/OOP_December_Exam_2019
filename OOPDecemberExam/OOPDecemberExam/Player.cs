82d3f1818bfa9756dc418ed4aca640f785f8a3fe
S00188447 <47103333+S00188447@users.noreply.github.com>
2019-12-11 10:22:24 +0000

    Properties and enum created

---
+++ OOPDecemberExam/OOPDecemberExam/Player.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPDecemberExam
+{
+    class Player
+    {
+
+     public string firstName { get; set; }
+     public string surName { get; set; }
+     public PlayerType Position { get; set; }
+     public DateTime DOB { get; set; }
+     public int age { get; set; }
+
+        public void Player()
+        {
+
+        }
+    }
+}
